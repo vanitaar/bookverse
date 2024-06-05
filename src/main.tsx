b6e2042aa5d4dd77b39b644b5fb2b48428021ad1
Vanitaa Rai <vanit.rai26@gmail.com>
2024-06-05 22:07:41 +0800

    feat config main for tanstack query and mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MantineProvider } from "@mantine/core";
 
 localStorage.debug = "BookVerse:*";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
