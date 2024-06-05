06777b58c8da5ef41c47fe5e64fb2c373d242270
Vanitaa Rai <vanit.rai26@gmail.com>
2024-06-05 12:06:33 +0800

    fix: database table schema

--- backend/config/setupDbSchema.js
+++ backend/config/setupDbSchema.js
@@ -4,25 +4,25 @@ const debug = require("debug")("BookVerse:config:dbSchema");
 const createTables = async () => {
   try {
     await pool.query(/* sql */ ` 
-CREATE TABLE IF NOT EXISTS Users (
+      CREATE TABLE IF NOT EXISTS Users (
         id SERIAL PRIMARY KEY,
-        username VARCHAR NOT NULL UNIQUE,
-        email VARCHAR NOT NULL UNIQUE,
-        password_hashed VARCHAR NOT NULL,
+        username VARCHAR(255) NOT NULL UNIQUE,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password_hashed VARCHAR(255) NOT NULL,
         role VARCHAR(50) NOT NULL CHECK (role IN ('reader', 'author'))
       );
 
       CREATE TABLE IF NOT EXISTS Series (
         id SERIAL PRIMARY KEY,
-        title VARCHAR NOT NULL,
-        status VARCHAR(50) CHECK (status IN ('incomplete', 'complete', 'standalone')),
-        author_id INT REFERENCES Users(id)
+        title VARCHAR(255) NOT NULL,
+        status VARCHAR(50) NOT NULL CHECK (status IN ('incomplete', 'complete', 'standalone')),
+        author_id INT REFERENCES Users(id) NOT NULL
       );
 
       CREATE TABLE IF NOT EXISTS Books (
         id SERIAL PRIMARY KEY,
-        title VARCHAR NOT NULL,
-        author_id INT REFERENCES Users(id),
+        title VARCHAR(255) NOT NULL,
+        author_id INT REFERENCES Users(id) NOT NULL,
         blurb TEXT,
         dedication TEXT,
         publication_date DATE,
@@ -37,26 +37,26 @@ CREATE TABLE IF NOT EXISTS Users (
 
       CREATE TABLE IF NOT EXISTS Follows (
         id SERIAL PRIMARY KEY,
-        reader_id INT REFERENCES Users(id),
-        author_id INT REFERENCES Users(id)
+        reader_id INT REFERENCES Users(id) NOT NULL,
+        author_id INT REFERENCES Users(id) NOT NULL
       );
 
       CREATE TABLE IF NOT EXISTS WatchSeries (
         id SERIAL PRIMARY KEY,
-        reader_id INT REFERENCES Users(id),
-        series_id INT REFERENCES Series(id)
+        reader_id INT REFERENCES Users(id) NOT NULL,
+        series_id INT REFERENCES Series(id) NOT NULL
       );
 
       CREATE TABLE IF NOT EXISTS Bookshelves (
         id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES Users(id),
-        book_id INT REFERENCES Books(id)
+        user_id INT REFERENCES Users(id) NOT NULL,
+        book_id INT REFERENCES Books(id) NOT NULL
       );
 
       CREATE TABLE IF NOT EXISTS BookNotes (
         id SERIAL PRIMARY KEY,
-        book_id INT REFERENCES Books(id),
-        user_id INT REFERENCES Users(id),
+        book_id INT REFERENCES Books(id) NOT NULL,
+        user_id INT REFERENCES Users(id) NOT NULL,
         rating INT CHECK (rating >= 1 AND rating <= 5),
         review_text TEXT,
         loan_status VARCHAR(50) CHECK (loan_status IN ('bought', 'loaned')),
@@ -65,9 +65,9 @@ CREATE TABLE IF NOT EXISTS Users (
 
       CREATE TABLE IF NOT EXISTS StatusUpdates (
         id SERIAL PRIMARY KEY,
-        author_id INT REFERENCES Users(id),
-        status TEXT,
-        archived BOOLEAN,
+        author_id INT REFERENCES Users(id) NOT NULL,
+        status TEXT NOT NULL,
+        archived BOOLEAN DEFAULT FALSE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
     `);
