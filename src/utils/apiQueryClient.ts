9983aaa7a6391b2111395b2c740865102903e9ec
Vanitaa Rai <vanit.rai26@gmail.com>
2024-06-07 12:29:07 +0800

    5th try deploy

--- src/utils/apiQueryClient.ts
+++ src/utils/apiQueryClient.ts
@@ -1,18 +1,18 @@
 import { QueryClient } from "@tanstack/react-query";
-import { authStore } from "../stores/authStore";
+// import { authStore } from "../stores/authStore";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       queryFn: async ({ queryKey }) => {
         const [url, options = {}] = queryKey as [string, RequestInit?]; // options set as obj and if undefined  set to empty obj --> allow ...
-        const { token } = authStore.getState(); //to directly access state from vanilla createStore
-
-        const response = await fetch(`http://localhost:5000${url}`, {
+        // const { token } = authStore.getState(); //to directly access state from vanilla createStore
+        const API_HOST = process.env.API_HOST || "http://localhost:5000";
+        const response = await fetch(`${API_HOST}${url}`, {
           ...options,
           headers: {
             ...options?.headers,
-            Authorization: token ? `Bearer ${token}` : "",
+            // Authorization: token ? `Bearer ${token}` : "",
           },
           credentials: "include",
         });
