048d037d894c78de0bc6aa25bef5748906475c4d
Vanitaa Rai <vanit.rai26@gmail.com>
2024-06-13 02:30:57 +0800

    feat styling mybooklisttab; checked deployment - succes

--- src/AuthPages/Dashboard/Author/MyBooklistTab.tsx
+++ src/AuthPages/Dashboard/Author/MyBooklistTab.tsx
@@ -36,20 +36,38 @@ const MyBooklistTab = () => {
 
   return (
     <div className="mt-20">
-      <h1>My Booklist</h1>
+      <h1 className="text-3xl font-bold mb-6">My Booklist</h1>
       <Link to="/dashboard/add-book">
-        <button>Add Book</button>
+        <button className="bg-teal-500 hover:bg-teal-600 text-white py-2 px-4 rounded-md mb-6">
+          Add Book
+        </button>
       </Link>
-      <ul>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {data &&
           data.map((book) => (
-            <li key={book.id}>
-              <h2>{book.title}</h2>
-              <p>{book.blurb}</p>
-              {book.image_url && <img src={book.image_url} alt={book.title} />}
-            </li>
+            <div key={book.id} className="bg-white shadow-md rounded-md p-4">
+              <h2 className="text-lg font-semibold mb-2 text-red-500">
+                {book.title}
+              </h2>
+              {book.image_url && (
+                <img
+                  src={book.image_url}
+                  alt={book.title}
+                  className="w-full h-40 object-cover mb-2 rounded-md"
+                />
+              )}
+              <p className="text-gray-700 mb-4">Blurb: {book.blurb}</p>
+              {/* <Link to={`/dashboard/edit-book/${book.id}`}>
+                <button className="bg-yellow-500 hover:bg-yellow-600 text-white py-1 px-2 rounded-md mr-2">
+                  Edit
+                </button>
+              </Link>
+              <button className="bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded-md">
+                Delete
+              </button> */}
+            </div>
           ))}
-      </ul>
+      </div>
     </div>
   );
 };
